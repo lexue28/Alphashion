ec98da040c0d3bcc774be4064a793c48378492fe
shadowdragon2805 <lexue28@gmail.com>
2022-06-26 07:32:31 -0400

    hi
    hi

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ import tkinter
 from dataPlotter import bar_graph
 from dataPlotter import pie_graph
 from machinelearning import ml_imageprediction
+from csvWorker import append_data
 
 
 
@@ -425,6 +426,7 @@ class ScreenSix(Screen):
             sizeID = self.ids.sizeID.text
             descriptionID = self.ids.descriptionID.text
             favID = self.ids.favID.text
+            append_data(typeID, statusID, priceID, sizeID, descriptionID, favID)
 
 
 screen_manager = ScreenManager()
